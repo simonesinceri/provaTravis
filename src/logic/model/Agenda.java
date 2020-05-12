c66557df563efc50090dd85749e7fee80f5bb07e
simonesinceri <56643807+simonesinceri@users.noreply.github.com>
2020-05-12 23:47:21 +0200

    riproviamo con travis

--- src/logic/model/Agenda.java
+++ src/logic/model/Agenda.java
@@ -3,7 +3,7 @@ package logic.model;
 import java.util.Vector;
 
 public class Agenda {
-	
+	// classe in cui gestiamo prenotazioni :cerca prenotaz, aggiungi, elimina,ecc.
 	private Vector<Reservation> vReservation;
 
 	public Agenda(Vector<Reservation> vReservation) { // questo costruttore potrebbe non servire
